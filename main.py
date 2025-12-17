76578dd0802ffa15a8fa49a217ae26925feed3cb
ryguy.os <ryletstolz@gmail.com>
2025-12-17 08:59:27 -0500

    Add LoginScreen to the application stack

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication, QStackedWidget
 from ui.boot import BootScreen
+from ui.login import LoginScreen
 from ui.desktop import Desktop
 
 app = QApplication(sys.argv)
@@ -8,10 +9,12 @@ app = QApplication(sys.argv)
 stack = QStackedWidget()
 
 boot = BootScreen(stack)
+login = LoginScreen(stack)
 desktop = Desktop()
 
-stack.addWidget(boot)
-stack.addWidget(desktop)
+stack.addWidget(boot)     # index 0
+stack.addWidget(login)    # index 1
+stack.addWidget(desktop)  # index 2
 
 stack.setCurrentWidget(boot)
 stack.showFullScreen()
